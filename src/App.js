8c0b1d4fec424b0ca6dae19928bc471a3e6eb852
Aman <15singh.aman@gmail.com>
2023-04-13 03:23:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import { Component } from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import About from "./About";
+
+
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+  );
+}
